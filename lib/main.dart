2606a481180ed34c0235d1c2ef05721b3d3ecb0c
abdellatif_ait <abdellatifaiteur@gmail.com>
2021-04-16 13:23:13 +0200

    Just trying

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+ class MyApp extends StatelessWidget {
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       home: Scaffold(
+         backgroundColor: Colors.black,
+         body: SafeArea(
+           child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+             children: <Widget>[
+               CircleAvatar(
+                 backgroundColor: Colors.white,
+               ),
+               Container(
+                 color: Colors.teal,
+                 height: 40.0,
+                 child: Row(
+                   children: <Widget>[
+                     Icon(Icons.phone,
+                       size: 12.0,
+                       color: Colors.black,
+                     ),
+                     SizedBox(
+                       width: 10.0,
+                     ),
+                     Text('Your phone number',
+                     style: TextStyle(
+                       fontSize: 20.0,
+                       color: Colors.black,
+                     ),
+                     ),
+
+                   ]
+                 ),
+               ),
+               Container(
+                 color: Colors.amberAccent,
+                 height: 40.0,
+                 padding: EdgeInsetsDirectional.only(start: 10.0,top: 0.0,end: 10.0,bottom: 0.0),
+                 child: Row(
+                     children: <Widget>[
+                       Icon(Icons.email,
+                         size: 12.0,
+                         color: Colors.black,
+                       ),
+                       SizedBox(
+                         width: 10.0,
+                       ),
+                       Text('Your email',
+                         style: TextStyle(
+                           fontSize: 20.0,
+                           color: Colors.black,
+                         ),
+                       ),
+
+                     ]
+                 ),
+               ),
+             ],
+             
+           ),
+         ),
+      )
+     );
+
+
+
+   }
+ }
